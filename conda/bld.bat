931f092ff34a3022c0b773816c526e15b0c99a9e
Patrick Snape <patricksnape@gmail.com>
2017-01-23 23:09:25 +0100

    Remove patches from windows

--- conda/bld.bat
+++ conda/bld.bat
@@ -30,11 +30,6 @@ IF %PY_MAJOR% EQU 3 (GOTO :PY3) else (GOTO :PY2)
     
     GOTO :NOTCPP11
     
-:NOTCPP11
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\kcftracker.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_beamsearch_decoder.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_hmm_decoder.patch"
-
 :PYTHON_SETUP
 
 set PY_LIB=python%PY_MAJOR%%PY_MINOR%.lib
